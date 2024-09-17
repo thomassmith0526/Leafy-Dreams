800cce803629f2b31937a0122d4b65e734274a0b
Josh Stringer <joshstringer@live.com>
2024-09-17 11:43:09 -0500

    fixed typeDefs and resolvers, graphql now runs

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,11 +2,11 @@ const { Bug, Helpful, Maintenance, Plant, StartUp, State } = require('../models'
 
 const resolvers = {
     Query: {
-
+        hello: () => 'Hello From Query!'
     },
 
     Mutation: {
-
+        hello: () => 'Hello From Mutation!',
     },
 };
 
