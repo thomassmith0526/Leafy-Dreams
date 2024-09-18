e581876645d2779e553e28ab16e7261b1542dfb6
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-09-17 19:55:38 -0500

    added just the Login form. Started the formhandler for it as well but havent finished

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 
+
+// const loginFormHandler = async (event) => {
+//     event.preventDefault();
+//     const email = document.querySelector('.email-login').value.trim()
+//     const password = document.querySelector('.password-login').value.trim();
+//     if (email && password) {
+//         const response =await fetch()
+//     }
+// }
 const Login = () => {
     return (
         <>
-            <h1>Login</h1>
+            <div class="mb-3">
+                <label class="form-label">Email:</label>
+                <input
+                    type="text"
+                    class=""
+                    name="email"
+                    id="email-login"
+                    placeholder="Enter Your Email"
+                />
+            </div>
+            
+            <div class="mb-3">
+                <label class="form-label">Password:</label>
+                <input
+                    type="password"
+                    class=""
+                    name="password"
+                    id="password-login"
+                    placeholder="Enter Your Password"
+                />
+            </div>
+            <button type='submit'>Login</button>
+
+            <h2>Don't have an account?</h2>
+            <button type='submit'>Sign-up</button>
         </>
     )
 }
