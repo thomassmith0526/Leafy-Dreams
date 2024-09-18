d68558ccc813bb9d67898774def0c657f613e439
Josh Stringer <joshstringer@live.com>
2024-09-18 15:20:01 -0500

    idea for the home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
+import Search from './pages/Search/Search';
 import AreaInfo from './pages/AreaInfo/AreaInfo';
 import PlantInfo from './pages/PlantInfo/PlantInfo';
 
@@ -12,31 +13,36 @@ function App() {
   return (
     <>
     <Router>
-      <h1>Josh Is A </h1>
+      <div className='headerNav'>
 
-        <Link to="/">
-          <button>Home</button>
-        </Link>
+        <div className='siteTitle'>
+        <img src='./src/assets/Animal_Crossing_Leaf.svg' alt='Leaf Icon' />
+          <h1>Leafy Dreams</h1>
+        </div>
 
-        <Link to="/signup">
-          <button>Sign Up</button>
-        </Link>
+        <div className='buttonBox'>
+          <Link to="/">
+            <button>Home</button>
+          </Link>
 
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
 
-        <Link to="/profile">
-          <button>Profile</button>
-        </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
 
-        <Link to="/area-info">
-          <button>Area Info</button>
-        </Link>
+          <Link to="/profile">
+            <button>Profile</button>
+          </Link>
 
-        <Link to="/plant-info">
-          <button>Plant Info</button>
-        </Link>
+          <Link to="/search">
+            <button>Search</button>
+          </Link>
+        </div>
+
+      </div>
 
         <Routes>
           <Route path="/" element={<Home />} />
@@ -46,6 +52,8 @@ function App() {
           <Route path="/login" element={<Login />} />
 
           <Route path="/profile" element={<Profile />} />
+
+          <Route path="/search" element={<Search />} />
 
           <Route path="/area-info" element={<AreaInfo />} />
 
---
+++ client/src/pages/Search/Search.jsx
@@ -0,0 +1,13 @@
+import './Search.css';
+
+const Search = () => {
+    return (
+        <>
+        <div className="searchBody">
+            <h1>Search</h1>
+        </div>
+        </>
+    )
+}
+
+export default Search;
