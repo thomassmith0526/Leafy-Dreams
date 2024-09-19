a2190e33bf18c1323874348768fcc6728254d9dd
smrsun4 <smrsun4@gmail.com>
2024-09-19 10:59:48 -0400

    wrote for resolvers.js plant query and mutation

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -1,12 +1,20 @@
-const { Bug, Helpful, Maintenance, Plant, StartUp, State } = require('../models');
+const { Plant, StartUp, State, Bug, Helpful, Maintenance } = require('../models');
 
 const resolvers = {
     Query: {
         hello: () => 'Hello From Query!'
+        // plant: async () => await Plant.find(), //add back in when ready 
     },
 
     Mutation: {
         hello: () => 'Hello From Mutation!',
+        // findPlant: async (_, { state, name }) => {
+        //     const newPlant = new Plant({ state, name });
+        //     return await newPlant.save();
+        // },
+        // updatePlant: async (_, { userId, state, name }) => {
+        //     return await Plant.findByIdAndUpdate(userId, { state, name }, { new: true});
+        // }
     },
 };
 
