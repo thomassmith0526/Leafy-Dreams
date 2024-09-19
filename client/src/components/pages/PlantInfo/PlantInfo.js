fd0b6a03a867d400442d0428ca60e90ccf370680
thomassmith0526 <thomassmith0526@gmail.com>
2024-09-19 15:58:33 -0500

    nope

--- client/src/components/pages/PlantInfo/PlantInfo.js
+++ client/src/components/pages/PlantInfo/PlantInfo.js
@@ -1,33 +1,33 @@
 // urls for plant data
 
-const SpeciesURL = 'https://perenual.com/api/species-list?key=';
+// const SpeciesURL = 'https://perenual.com/api/species-list?key=';
 
-const PlantDetailsURL = 'https://perenual.com/api/species/details/[ID]?key=';
+// const PlantDetailsURL = 'https://perenual.com/api/species/details/[ID]?key=';
 
-const PlantDiseaseURL = 'https://perenual.com/api/pest-disease-list?key=';
+// const PlantDiseaseURL = 'https://perenual.com/api/pest-disease-list?key=';
 
-const PlantGuidesURL = 'https://perenual.com/api/species-care-guide-list?key=';
+// const PlantGuidesURL = 'https://perenual.com/api/species-care-guide-list?key=';
 
-// Global variables for catagories that the api will fetch
+// // Global variables for catagories that the api will fetch
 
-const PlantImage = localStorage.getItem("small_url");
-const PlantName = localStorage.getItem("common_name");
-const PlantColor = localStorage.getItem("color");
-const PlantType = localStorage.getItem("type");
-const SunExposure = localStorage.getItem("sunlight");
-const Watering = localStorage.getItem("watering");
-const GrowthRate = localStorage.getItem("growth_rate");
-const PoisonousHumans = localStorage.getItem("poisonous_to_humans");
-const PoisonousPets = localStorage.getItem("poisonous_to_pets");
-const Flowers = localStorage.getItem("flowers");
-const Indoors = localStorage.getItem("indoor");
-const Medicinal = localStorage.getItem("medicinal");
-const EdibleLeaf = localStorage.getItem("edible_leaf");
+// const PlantImage = localStorage.getItem("small_url");
+// const PlantName = localStorage.getItem("common_name");
+// const PlantColor = localStorage.getItem("color");
+// const PlantType = localStorage.getItem("type");
+// const SunExposure = localStorage.getItem("sunlight");
+// const Watering = localStorage.getItem("watering");
+// const GrowthRate = localStorage.getItem("growth_rate");
+// const PoisonousHumans = localStorage.getItem("poisonous_to_humans");
+// const PoisonousPets = localStorage.getItem("poisonous_to_pets");
+// const Flowers = localStorage.getItem("flowers");
+// const Indoors = localStorage.getItem("indoor");
+// const Medicinal = localStorage.getItem("medicinal");
+// const EdibleLeaf = localStorage.getItem("edible_leaf");
 
 // Variables for API
 // Adding both api keys's just in case we decide to pull information from both
 
-'X-RAPIDAPI-KEY':import.meta.env.VITE_TREFLEAPI_KEY
-'X-RAPIDAPI-KEY':import.meta.env.VITE_PERENUALAPI_KEY
+// 'X-RAPIDAPI-KEY':import.meta.env.VITE_TREFLEAPI_KEY
+// 'X-RAPIDAPI-KEY':import.meta.env.VITE_PERENUALAPI_KEY
 
 // Functions: Check to see what information can be pulled. 
