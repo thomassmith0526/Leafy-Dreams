e41a19e42954d627a182c6192ea420bb5d79777c
thomassmith0526 <thomassmith0526@gmail.com>
2024-09-21 15:43:14 -0500

    css for profile

--- client/src/components/pages/Profile/Profile.jsx
+++ client/src/components/pages/Profile/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import '../Profile/Profile.css'
 import SearchBarArea from '../../SearchBarArea';
 import SearchBarPlant from '../../SearchBarPlants';
 
 const Profile = () => {
     return (
-        <main> 
+        <main>
             <h1>Profile</h1>
             <div className='Location'>
                 <SearchBarArea></SearchBarArea>
@@ -13,11 +13,21 @@ const Profile = () => {
             <div className='Planet'>
                 <SearchBarPlant></SearchBarPlant>
             </div>
-            <div className='Recent'> 
-                 <h2>Recent Searches</h2> 
-            </div>
+           
+                <div className='Recent'>
+                    <h2>Recent Searches</h2>
+                    <ul>
+                        <li>rose</li>
+                        <li>rose</li>
+                        <li>rose</li>
+                        <li>rose</li>
+                    </ul>
+                </div>
+            
+
             <div className='History'>
                 <h2>Your Zone</h2>
+                <h3>zone ##</h3>
             </div>
         </main>
     )
