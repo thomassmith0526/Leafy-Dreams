83fb83fc3a4cc9a353557412afce2a89f2b7b64f
Josh Stringer <joshstringer@live.com>
2024-09-23 18:50:36 -0500

    updated app wide styling and home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import './App.css';
-
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './components/pages/Home/Home';
 import Signup from './components/pages/Signup/Signup';
 import Login from './components/pages/Login/Login';
 import Profile from './components/pages/Profile/Profile';
 import AreaInfo from './components/pages/AreaInfo/AreaInfo';
-import PlantInfo from './components/pages/PlantInfo/PlantInfo.jsx'
+import PlantInfo from './components/pages/PlantInfo/PlantInfo.jsx';
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
 });
@@ -18,26 +16,20 @@ const client = new ApolloClient({
 function App() {
   return (
     <>
-      <ApolloProvider client={client}>
-        <Router>
-          <div className='Company'>
-            <h1>Leafy Dreams </h1>
-          </div>
+    <ApolloProvider client={client}>
+    <Router>
+      <div className='headerNav'>
+
+        <div className='siteTitle'>
+        <img src='./src/assets/images/App/Animal_Crossing_Leaf.svg' alt='Leaf Icon' />
+          <h1>Leafy Dreams</h1>
+        </div>
+
+        <div className='buttonBox'>
           <Link to="/">
             <button>Home</button>
           </Link>
 
-          <Link to="/profile">
-            <button>Profile</button>
-          </Link>
-
-          <Link to="/area-info">
-            <button>Area Info</button>
-          </Link>
-
-          <Link to="/plant-info">
-            <button>Plant Info</button>
-          </Link>
           <Link to="/signup">
             <button>Sign Up</button>
           </Link>
@@ -46,23 +38,31 @@ function App() {
             <button>Login</button>
           </Link>
 
-          <Routes>
-            <Route path="/" element={<Home />} />
+          <Link to="/profile">
+            <button>Profile</button>
+          </Link>
+        </div>
 
-            <Route path="/profile" element={<Profile />} />
+      </div>
 
-            <Route path="/area-info" element={<AreaInfo />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-            <Route path="/plant-info" element={<PlantInfo />} />
-            
-            <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
 
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </Router>
-      </ApolloProvider>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="/profile" element={<Profile />} />
+
+          <Route path="/area-info" element={<AreaInfo />} />
+
+          <Route path="/plant-info" element={<PlantInfo />} />
+        </Routes>
+    </Router>
+    </ApolloProvider>
     </>
   )
 }
 
 export default App;
+
