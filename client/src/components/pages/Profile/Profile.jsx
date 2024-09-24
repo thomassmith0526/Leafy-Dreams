7a42dc2832addd3c71539d0b8deff778381b20d8
MommaDLP <159870485+MommaDLP@users.noreply.github.com>
2024-09-23 21:01:42 -0500

    final profile jsx user info added now to add styling

--- client/src/components/pages/Profile/Profile.jsx
+++ client/src/components/pages/Profile/Profile.jsx
@@ -8,7 +8,7 @@ import SearchBarPlant from '../../SearchBarPlants/index';
 const Profile = () => {
     
     const [searchParams]= useSearchParams();
-    const searchPlant = searchParams.get('search')
+    const searchPlant = searchParams.get('search');
     const [plant, setPlant] = useState(null);
 
 
@@ -151,8 +151,48 @@ const Profile = () => {
                 <h2>Your Zone</h2>
                 <h3>zone ##</h3>
             </div>
+            </div>
+
+              {/* Personal Information Section */}
+              <div className="col-md-4 hidden-xs hidden-sm">
+                    <ul className="profile-info-list">
+                        <li className="title">PERSONAL INFORMATION</li>
+                        <li>
+                            <div className="field">Occupation:</div>
+                            <div className="value">UXUI / Frontend Developer</div>
+                        </li>
+                        <li>
+                            <div className="field">Skills:</div>
+                            <div className="value">C++, PHP, HTML5, CSS, jQuery, MYSQL, Ionic, Laravel, Phonegap, Bootstrap, Angular JS, Asp.net</div>
+                        </li>
+                        <li>
+                            <div className="field">Birth of Date:</div>
+                            <div className="value">1989/11/04</div>
+                        </li>
+                        <li>
+                            <div className="field">Country:</div>
+                            <div className="value">San Francisco</div>
+                        </li>
+                        <li>
+                            <div className="field">Address:</div>
+                            <div className="value">
+                                <address className="m-b-0">
+                                    Twitter, Inc.<br />
+                                    1355 Market Street, Suite 900<br />
+                                    San Francisco, CA 94103
+                                </address>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="field">Phone No.:</div>
+                            <div className="value">
+                                (123) 456-7890
+                            </div>
+                        </li>
+                    </ul>
+                </div>
         </main>
     );
-};
+}
 
 export default Profile;
