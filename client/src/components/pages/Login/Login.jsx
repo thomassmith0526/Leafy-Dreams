1c20726bf0a28ee916708c3749c28ef1492a4d81
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-09-24 16:58:39 -0500

    added fix

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -96,7 +96,7 @@ const Login = () => {
         
                 ) : (
                     <div>
-                        <h1>You're Already Logged In</h1>
+                        <h1>You're already logged in!</h1>
                         <h2>Would you like to logout?</h2>
                         <button onClick={handleLogout}>Logout</button>
                     </div>                
