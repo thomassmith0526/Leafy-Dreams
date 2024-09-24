10c5a781a20b32ba0c8c868e9aa6c32f243e9f40
smrsun4 <smrsun4@gmail.com>
2024-09-24 16:01:35 -0400

    commiting staged changes

--- client/src/components/pages/Login/Login.jsx
+++ client/src/components/pages/Login/Login.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useContext } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useQuery, gql } from '@apollo/client';
 import '../Signup/Signup.css'
 
 const GET_SINGLE_USER = gql `
