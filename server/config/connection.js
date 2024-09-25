a734d47700aa28b3d76deca076ae1f2848195840
Josh Stringer <joshstringer@live.com>
2024-09-24 20:41:15 -0500

    didnt quite put everything in that i needed

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/plant_db';
+const mongoURI = process.env.NODE_ENV === 'production'
+    ? 'mongodb+srv://joshstringer:vHwMvfyHljdttfgC@plantcluster.jfen1.mongodb.net/team_plant_db?retryWrites=true&w=majority&appName=PlantCluster'
+    : 'mongodb://localhost:27017/plant_db';
 
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
